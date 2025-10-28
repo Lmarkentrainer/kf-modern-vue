e8cde483a273bff6029b9533dc2ac5adc7103ef6
Lmarkentrainer <l.revestido@markentrainer.de>
2025-10-28 15:31:04 +0100

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/kf-modern-vue/",
   server: {
     host: "::",
     port: 8080,
